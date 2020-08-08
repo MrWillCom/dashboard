2176cc332b9c6e00433063490889e1a0c8f156b9
MrWillCom <mr.will.com@outlook.com>
2020-08-08 18:47:42 +0800

    Update

--- index.js
+++ index.js
@@ -1,24 +1,76 @@
+var stdin = process.openStdin();
+stdin.setRawMode(true);
+stdin.resume()
+stdin.setEncoding('utf8')
+
 const align = require('./components/align')
 
 const cons = {
     clear: () => { console.clear() },
     log: (data) => { process.stdout.write(data) },
-    input: (input, listener) => {
-        process.stdin.on('data', (data) => {
-            // For different platforms: CRLF, CR, LF
-            if (data.toString() == `${input}\r\n` || data.toString() == `${input}\r` || data.toString() == `${input}\n`) { listener(data) }
-        })
-    },
     width: () => { return process.stdout.columns },
     height: () => { return process.stdout.rows },
 }
 
 cons.clear()
 
-cons.log('Dashboard')
-for (let i = 0; i < cons.height() - 2; i++) {
-    cons.log('\n')
-}
-cons.log('> ')
+var navHeight = 1
+var commandBarHeight = 0
+var commandBarShow = false
 
-cons.input('exit', () => { process.exit(0) })
+function display() {
+    cons.clear()
+    cons.log('Dashboard')
+    for (let i = 0; i < cons.height() - navHeight - commandBarHeight; i++) {
+        cons.log('\n')
+    }
+    if (commandBarShow) {
+        cons.log('> ')
+    }
+}
+
+function setCommandBarShow(value) {
+    if (value == true) {
+        commandBarHeight = 1
+        commandBarShow = true
+    }
+    else {
+        commandBarHeight = 0
+        commandBarShow = false
+    }
+    display()
+}
+
+function runCommand(command) {
+    switch (command) {
+        case 'exit':
+            process.exit(0)
+            break;
+        default:
+            setCommandBarShow(false)
+    }
+}
+
+var inputStream = ''
+
+stdin.on('data', function (key) {
+    if (key == '\u0003') {
+        process.exit();
+    }
+    if (key == '\r\n' || key == '\r' || key == '\n') {
+        runCommand(inputStream)
+    }
+    if (commandBarShow) {
+        process.stdout.write(key);
+
+        inputStream = inputStream + key
+    }
+    if (key == '>') {
+        setCommandBarShow(true)
+    }
+    if (key == '<') {
+        setCommandBarShow(false)
+    }
+})
+
+display()
